fefa8b6469795fc088704c6979ea02de9d306919
Todd Judd <toddjudd@gmail.com>
2024-12-11 16:44:44 -0700

    day 10

--- src/2024/10/solution.rb
+++ src/2024/10/solution.rb
@@ -1,9 +1,145 @@
 require_relative '../../CalendarDate.rb'
 
+class Directions
+  class << self
+    def up
+      [0,-1]
+    end
+    def down
+      [0,1]
+    end
+    def left
+      [-1,0]
+    end
+    def right
+      [1,0]
+    end
+
+    def all
+      [
+        self.up,
+        self.right,
+        self.down,
+        self.left,
+      ]
+    end
+
+    def lookupString(direction)
+      case direction
+      when self.up
+        return "up"
+      when self.down
+        return "down"
+      when self.left
+        return "left"
+      when self.right
+        return "right"
+      else
+        return "unknown"
+      end
+    end
+  end
+end
+
 class Solution < CalendarDate
   def initialize
     super(10)
+
+#     @data = "
+# 89010123
+# 78121874
+# 87430965
+# 96549874
+# 45678903
+# 32019012
+# 01329801
+# 10456732"
+
+    @data = @data.strip!.split("\n").map {|x| x.split('').map { |y| y.to_i}} # map data
+
+    @paths = {}
+
+    puts "Data: #{@data}"
+
+    @trailheads = searchMap(0)
+    @peaks = searchMap(9)
+
+    puts "Trailheads: #{@trailheads}"
+    puts "Peaks: #{@peaks}"
+
+    # trailhead = @trailheads[4]
+    #   puts "Trailhead: #{trailhead}"
+    #   blaze(trailhead, 0, trailhead)
+    #   puts "Paths: #{@paths[trailhead.to_s]}"
+    #   puts "uniqPaths #{@paths[trailhead.to_s].uniq.count}"
+    #   puts "totalPaths #{@paths[trailhead.to_s].count}"
+
+
+    @trailheads.each do |trailhead|
+      puts "Trailhead: #{trailhead}"
+      blaze(trailhead, 0, trailhead)
+      puts "uniqPaths #{@paths[trailhead.to_s].uniq.count}"
+      puts "totalPaths #{@paths[trailhead.to_s].count}"
+    end
+
+    uniqPathSum = 0
+    totalPathSum = 0
+    @paths.each do |key, value|
+      uniqPathSum += value.uniq.count
+      totalPathSum += value.count
+    end
+
+    puts "uniq Paths: #{uniqPathSum}"
+    puts "total Paths: #{totalPathSum}"
+
+
   end
+
+  def move(x,y,direction)
+    xd = direction[0]
+    yd = direction[1]
+
+    x1 = x + xd
+    y1 = y + yd
+
+    if x1 < 0 || y1 < 0 || x1 > @data.length-1 || y1 > @data[x1].length-1
+      return [-1,-1,-1]
+    end
+    return [@data[y1][x1], x1,y1]
+  end
+
+  def searchMap(query)
+        points = []
+    @data.each_with_index do |row, y|
+      row.each_with_index do |value, x|
+        points << [x,y] if value == query
+      end
+    end
+    return points
+  end
+
+  def blaze(point, value, trailhead)
+    # puts "Blazing from #{point[0]}, #{point[1]} Current Value: #{value}"
+    x,y = point[0],point[1]
+
+    Directions.all.each do |direction|
+      if value == 9
+        if @paths[trailhead.to_s]
+          @paths[trailhead.to_s] << point.to_s
+          return
+        end
+        @paths[trailhead.to_s] =[point.to_s]
+        return
+      end
+      neighbor = move(x,y,direction)
+      if neighbor[0] == value+1
+        # puts "Neighbor #{Directions.lookupString(direction)}"
+        blaze([neighbor[1],neighbor[2]], value+1, trailhead)
+      end
+    end
+  end
+
+
 end
 
 Solution.new
--- src/2024/11/solution.rb
+++ src/2024/11/solution.rb
@@ -1,9 +1,74 @@
 require_relative '../../CalendarDate.rb'
 
+class Integer
+  def even_digits?
+    self.digits.even?
+  end
+
+  def digits
+    Math.log10(self).to_i + 1
+  end
+end
+
 class Solution < CalendarDate
   def initialize
     super(11)
+
+    @stoneCount = 0
+
+    # @data = '125 17'.split(' ').map {|x| x.to_i} # Test Data
+    @data = @data.split(' ').map {|x| x.to_i} # map data
+    # @testAnswers = [3,4, 5, 9, 13, 22]
+    @blinks = 75
+
+    @mathCache = {0=>1} # stoneValue => result same math will always be applied
+    # part of me want's to prbuild the cache. I wonder what efficiency gains that would have.
+    @scoreCache = {} # "value,blinks" => result
+
+
+    puts "Data: #{@data}"
+    puts "Blinks: #{@blinks}"
+    # puts "Expected: #{@testAnswers[@blinks-1]}"
+
+    @data.each do |value|
+      @stoneCount += score(value, 1)
+    end
+
+    puts "Stone Count: #{@stoneCount}"
   end
+
+  def applyRule(value) # nubmer => [int] | int
+    return @mathCache[value] if @mathCache[value]
+
+    if value.even_digits?
+      lenght = value.digits
+      a = value / 10**(lenght/2)
+      b = value % 10**(lenght/2)
+      @mathCache[value] = [a,b]
+      return [a,b]
+    end
+
+    product = value*2024
+
+    @mathCache[value] = product
+    return product
+
+  end
+
+  def score(value, blinks) # int, int => int
+    return @scoreCache["#{value},#{blinks}"] if @scoreCache["#{value},#{blinks}"]
+
+    return 1 if blinks > @blinks
+
+    ruleResult = applyRule(value)
+
+    if ruleResult.is_a? Array
+       @scoreCache["#{value},#{blinks}"]  = score(ruleResult[0], blinks+1) + score(ruleResult[1], blinks+1)
+    else
+       @scoreCache["#{value},#{blinks}"]  = score(ruleResult, blinks+1)
+    end
+  end
+
 end
 
 Solution.new
